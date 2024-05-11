150c71819fda28ce7e1e55497ff7a4c6ca6463e0
thejasveer13 <thejasveer13@gmail.com>
2024-05-11 06:36:01 -0500

    Added copy animation

--- frontend/src/components/ReadingListIcon.tsx
+++ frontend/src/components/ReadingListIcon.tsx
@@ -1,4 +1,4 @@
-import {   useMemo } from "react"
+import {   useEffect, useMemo, useRef, useState } from "react"
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import { userAtom } from "../store/userAtom"
 import { useReadingList } from "../hooks/apis"
@@ -13,6 +13,7 @@ export const ReadingListIcon = ({id , col=true}:{id: string,col?:boolean})=>{
      const {list} = useRecoilValue(userAtom)
      const {update,loading} = useReadingList();
      const setAlert= useSetRecoilState(alertAtom)
+     const [copiedClass,setCopiedClass]= useState(false)
      const user = useRecoilValue(userAtom)
      const navigate = useNavigate()
        const handleReadingList = ()=>{
@@ -23,8 +24,17 @@ export const ReadingListIcon = ({id , col=true}:{id: string,col?:boolean})=>{
         }
                
         } 
-        const copy = async ()=>{
-      
+        useEffect(()=>{
+            let timerId = setTimeout(()=>{
+                setCopiedClass(false)
+               },2000)
+
+               return ()=> clearTimeout(timerId)
+         
+        },[copiedClass])
+        const copy = async (e)=>{
+            setCopiedClass(true)
+           
             const domain = window.location.host;
             await navigator.clipboard.writeText(`${domain}/blog/${id}`);
             setAlert([{msg: 'Link copied',bgColor:'bg-black'}])
@@ -39,9 +49,16 @@ export const ReadingListIcon = ({id , col=true}:{id: string,col?:boolean})=>{
 
 
     return     <div className={ `flex ${col ? 'flex-col': ''}  gap-5 text-slate-400`}> 
-                        <svg onClick={copy} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                        <div className="relative">
+                            {copiedClass?   <div className="absolute -ml-16 bg-white animate-fade-left z-10">copied</div> : ''}
+                         
+                        <svg onClick={copy} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${copiedClass?'fill-slate-800 size-5 z-20':''} size-6 cursor-pointer `}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                      
                         </svg>
+                        </div>
+                       
+
                      {loading ? <ButtonSpinner  /> :    <svg onClick={handleReadingList}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6  cursor-pointer ${exist? 'fill-slate-400':''}`}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                             </svg>}
@@ -50,4 +67,8 @@ export const ReadingListIcon = ({id , col=true}:{id: string,col?:boolean})=>{
           
 }
 
+
+function useeState() {
+    throw new Error("Function not implemented.")
+}
  
