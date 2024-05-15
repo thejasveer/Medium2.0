df71f62c57afbd734cb821ab1f7aa84894e17073
thejasveer13 <thejasveer13@gmail.com>
2024-05-15 04:55:16 -0500

    minified build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,16 @@
-// import { defineConfig,loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig,loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
 
-// // https://vitejs.dev/config/
-// export default defineConfig(() => {
+// https://vitejs.dev/config/
+export default defineConfig(() => {
  
-//   return {
+  return {
      
-//     build: {
-//       minify: false // Disable minification
-//     },
-//     plugins: [react()],
-//   }
-// })
-
-// vite.config.ts
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Other configuration options...
-
-  build: {
-    minify: false // Disable minification
+   
+    plugins: [react()],
   }
-});
+})
+
+ 
