b2234c5dcc174f971aaf93cdc622a799817ee93b
thejasveer13 <thejasveer13@gmail.com>
2024-05-19 07:09:44 -0500

    minor fix

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_URL="https://my-app.thejasveer13.workers.dev/api/v1"
-export const BACKEND_URL="http://localhost:8787/api/v1"
+export const BACKEND_URL="https://my-app.thejasveer13.workers.dev/api/v1"
+// export const BACKEND_URL="http://localhost:8787/api/v1"
  
